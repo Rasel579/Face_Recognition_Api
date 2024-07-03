087a96bbcc7b9692b87e2880648866908f5a0a27
Ruslan Shaykh <ruslanshaykh@yandex.ru>
2024-07-03 11:44:49 +0500

    init

---
+++ src/test/java/com/diplom/faces_recognition/FacesRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.faces_recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacesRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
