22209c9a06b9734eb69cc9577683d4c55a9854f1
RuslanShaykh <ruslanshaykh@yandex.ru>
2025-03-18 08:52:39 +0500

    yolo путь к моделе поменял

--- src/main/java/com/diplom/faces_recognition/nets/yolo/YOLOPretrained.java
+++ src/main/java/com/diplom/faces_recognition/nets/yolo/YOLOPretrained.java
@@ -40,7 +40,7 @@ import java.util.Random;
 
 public class YOLOPretrained {
     private static final long seed = 12345;
-    private static final String TRAIN_PATH = "./src/main/resources/static/yolo2_dl4j_inference.v11724589511596.zip";
+    private static final String TRAIN_PATH = "./src/main/resources/static/yolo2_dl4j_inference.v1.zip";
     private static final String TRAINED_PATH = "./src/main/resources/static/yolo2_dl4j_inference.v1";
     private static final String TRAIN_DATA_VOC = "D:/downloads/Microsoft_COCO.v2-raw-voc/short_train";
     private static final String TEST_DATA_VOC = "D:/downloads/Microsoft_COCO.v2-raw-voc/short_valid";
